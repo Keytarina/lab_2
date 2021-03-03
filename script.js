daa483acd997dfcb9ba06d7712d06619ca32f91c
Oleksiy Nosov <oleksiynosov@gmail.com>
2021-03-03 15:50:10 +0200

    Created Vegenere cipher functions

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+const phrase = 'attackatdawn';
+const key = 'lemon';
+
+
+function cipherMyText(text, key) {
+	let newPhrase = '';
+
+	for(let i = 0; i < text.length; i++) {
+		newPhrase += cipherLetter(text[i], key[i % key.length]);
+
+	}
+
+	return newPhrase;
+}
+
+function decipherMyText(text, key) {
+	let decriphedPhrase = '';
+
+	for(let i = 0; i < text.length; i++) {
+		decriphedPhrase += decipherLetter(text[i], key[i % key.length]);
+	}
+
+	return decriphedPhrase;
+}
 
 function cipherLetter(letter, key) {
 	let shift_l = alphabet.indexOf(letter);
 	let shift_k = alphabet.indexOf(key);
-	console.log(shift_l, shift_k);
 
-	let shift = shift_l + shift_k % alphabet.length;
-	
-	// if(alphabet[shift] == undefined) {
-	// 	shift = shift - alphabet.length; 
+	let shift = (shift_l + shift_k) % alphabet.length;
+
+	// 	shift = shift - alphabet.length;
 	// }
-	console.log(shift);
+	// console.log(shift);
 
-	return alphabet[shift]; 
+	return alphabet[shift];
 }
 
-function decryption(letter, key) {
+function decipherLetter(letter, key) {
 	let shift_l = alphabet.indexOf(letter);
 	let shift_k = alphabet.indexOf(key);
-	
 
+	let shift = (shift_l - shift_k + 26) % alphabet.length;
+
+	return alphabet[shift];
 }
 
 console.log(alphabet);
-// console.log(cipherLetter('b', 'f'));
-console.log(cipherLetter('f', 'x'));
-console.log(decryption('c', 'x'));
+
+const cypheredText = cipherMyText(phrase, key);
+
+console.log(cypheredText);
+console.log(decipherMyText(cypheredText, key))
+
+
+
